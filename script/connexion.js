030727ca00846f30fb10279ba55be54d8d03d119
Mattneox <112353509+Mattneox@users.noreply.github.com>
2023-02-07 16:39:57 +0100

    Add files via upload

---
+++ script/connexion.js
@@ -0,0 +1,38 @@
+
+// Connexion pour acceder à la page home.html
+function Login(){
+
+    //Recuperation de la valeurs de l'imput id
+    var username= document.login.id.value;
+    //Recuperation de la valeur de l'imput mdp
+    var password= document.login.mdp.value;
+    
+    //Verification du pseudo et du mot de passe
+    if (username=="mattneox" && password=="matt") {
+        //Si les deux sont bon redirection vers home.html
+        window.location="home.html";
+    }
+}
+
+
+// Recherer par une valeur envoyer dans la barre de recherche 
+function ouvrirIndex()
+{
+    //Recuperation de la valeur contenue dans search
+    var a = document.getElementById("search").value;
+    if (a == 'picture')
+    {
+        window.location  = "picture.html";
+    }
+}
+
+
+function darkMode()
+{
+    picture.src =
+    'pictures/fond/fond-technologie-numerique-fil-maille-reseau_1017-27428-modified.jpg';    
+}
+function Screen()
+{
+    document.documentElement.requestFullscreen();
+}
